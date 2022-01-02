29a1ed2d940a91f27ea6a6407d35c044487a8e2e
fuatmuh <fuat.muh@gmail.com>
2022-01-02 15:27:48 +0000

    first commit

---
+++ src/test/java/com/cybertek/tests/ChercherTest.java
@@ -0,0 +1,36 @@
+package com.cybertek.tests;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ChercherTest {
+
+    WebDriver driver;  //declare our reference for the object
+    @BeforeMethod
+    public void setUp(){
+        driver= WebDriverFactory.getDriver("chrome"); //create the object
+        driver.manage().window().maximize();
+
+        // implicitly wait, this is going to be applayed to while test casese and element
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        //driver.close();
+    }
+
+    @Test
+    public void alertPresentTest(){
+
+
+    }
+
+
+}
---
+++ src/test/java/com/cybertek/utilities/WebDriverFactory.java
@@ -0,0 +1,40 @@
+package com.cybertek.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class WebDriverFactory {
+
+    //task:
+    // Bu class standart yaptığımız işlemleri method şeklinde yazmamıza yarıyor.
+
+    public static WebDriver getDriver(String browsertype){
+
+        WebDriver driver=null;
+
+        switch (browsertype.toLowerCase()){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver=new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver=new FirefoxDriver();
+                break;
+            case "explorer":
+                WebDriverManager.edgedriver().setup();
+                driver=new EdgeDriver();
+                break;
+            case "safari":
+                WebDriverManager.safaridriver().setup();
+                driver=new SafariDriver();
+                break;
+        }
+        return driver;
+    }
+
+}
